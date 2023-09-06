7635ab30ea8723e7360bc067ae4fd619f5bda454
Wallace <wallaceste@hotmail.com>
2023-09-06 18:58:09 -0300

    Commit Final Mock

---
+++ src/test/java/dio/mockito/exemplos/CadastrarPessoaTest.java
@@ -0,0 +1,37 @@
+package dio.mockito.exemplos;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+@ExtendWith(MockitoExtension.class)
+public class CadastrarPessoaTest {
+
+    @Mock
+    private ApiDosCorreios apiDosCorreios;
+
+    @InjectMocks
+    private CadastrarPessoa cadastrarPessoa;
+
+    @Test
+    void validarDadosDeCadastro() {
+        DadosLocalizacao dadosLocalizacao = new DadosLocalizacao("RJ", "Maricá", "Av Beira Rio", "antiga rua do canal", "Centro");
+        Mockito.when(apiDosCorreios.buscaDadosComBaseNoCep("24900120")).thenReturn(dadosLocalizacao);
+        Pessoa pessoa = cadastrarPessoa.cadastrarPessoa("João", "98656554654654", LocalDate.of(2005, 1, 30), "24900120");
+
+        assertEquals("João", pessoa.getNome());
+        assertEquals("98656554654654", pessoa.getDocumento());
+        assertEquals(LocalDate.of(2005, 1, 30), pessoa.getNascimento());
+        assertEquals("RJ", pessoa.getEndereco().getUf());
+        assertEquals("Maricá", pessoa.getEndereco().getCidade());
+
+    }
+
+}
